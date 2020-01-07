2e68c16047ae75f93abb925e5430f379306eb4d2
Tony Nguyen <tonynguyen201@gmail.com>
2020-01-07 14:51:37 -0600

    Add initial pokemon card call from api

--- src/components/main/PokemonCard.js
+++ src/components/main/PokemonCard.js
@@ -1,7 +1,13 @@
 import React, { Component } from 'react';
 
 export default class PokemonCard extends Component {
+  state = {
+    name: '',
+    imageUrl: '',
+    pokemonIndex: ''
+  };
   render() {
+    const { name, url } = this.props;
     return (
       <div className=' px-4 py-2 max-w-sm rounded bg-gray-400 overflow-hidden shadow-lg mb-4'>
         <img
@@ -9,7 +15,7 @@ export default class PokemonCard extends Component {
           src='https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/1.png'
           alt=''
         />
-        <div className='font-bold text-l text-base'>Bulbasaur</div>
+        <div className='font-bold text-l text-base'>{name}</div>
         <p className='text-gray-700 text-base'>
           Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus
           quia, nulla! Maiores et perferendis eaque, exercitationem praesentium
--- src/components/main/PokemonList.js
+++ src/components/main/PokemonList.js
@@ -1,20 +1,29 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 
 import PokemonCard from './PokemonCard';
 
 export default class PokemonList extends Component {
+  state = {
+    url: 'https://pokeapi.co/api/v2/pokemon/',
+    pokemon: null
+  };
+
+  async componentDidMount() {
+    const res = await axios.get(this.state.url);
+    this.setState({ pokemon: res.data['results'] });
+  }
+
   render() {
     return (
       <div className='w-full flex flex-wrap justify-around'>
-        <PokemonCard />
-        <PokemonCard />
-        <PokemonCard />
-        <PokemonCard />
-        <PokemonCard />
-        <PokemonCard />
-        <PokemonCard />
-        <PokemonCard />
-        <PokemonCard />
+        {this.state.pokemon ? (
+          this.state.pokemon.map(poke => (
+            <PokemonCard key={poke.name} name={poke.name} url={poke.url} />
+          ))
+        ) : (
+          <div>There are no pokemon</div>
+        )}
       </div>
     );
   }
