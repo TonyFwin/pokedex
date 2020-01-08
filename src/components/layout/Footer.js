506d4280be4159f9babfa31d919b3f55a65fe990
Tony Nguyen <tonynguyen201@gmail.com>
2020-01-08 14:40:16 -0600

    style updates

--- src/components/layout/Footer.js
+++ src/components/layout/Footer.js
@@ -3,19 +3,30 @@ import React, { Component } from 'react';
 export default class Footer extends Component {
   render() {
     return (
-      <div className='container mt-auto mr-auto ml-auto'>
+      <footer className='container mt-auto mr-auto ml-auto'>
         <div className='flex justify-around'>
           <p>
-            Data from <a href='https://pokeapi.co/'>PokeAPI</a>
+            Data from{' '}
+            <a
+              rel='noopener noreferrer'
+              target='_blank'
+              href='https://pokeapi.co/'
+            >
+              PokeAPI
+            </a>
           </p>
           <p>
-            Pokeball icon by
-            <a href='https://www.flaticon.com/authors/nikita-golubev'>
+            Pokeball icon by{' '}
+            <a
+              rel='noopener noreferrer'
+              target='_blank'
+              href='https://www.flaticon.com/authors/nikita-golubev'
+            >
               Nikia Golubev
             </a>
           </p>
         </div>
-      </div>
+      </footer>
     );
   }
 }
